381a2945aea62251b13aa8767c3a13fc5aa61280
lentatyka <kentatyka@gmail.com>
2022-06-02 19:20:46 +0500

    activity_main.xml. A little fix align bottom panel.

--- app/src/main/java/com/lentatyka/focusstartproject/data/network/RateRepositoryImpl.kt
+++ app/src/main/java/com/lentatyka/focusstartproject/data/network/RateRepositoryImpl.kt
@@ -3,7 +3,8 @@ package com.lentatyka.focusstartproject.data.network
 import com.lentatyka.focusstartproject.domain.network.RateRepository
 import javax.inject.Inject
 
-class RateRepositoryImpl @Inject constructor(private val exchangeRatesApi: ExchangeRatesApi):
-    RateRepository {
+class RateRepositoryImpl @Inject constructor(
+    private val exchangeRatesApi: ExchangeRatesApi
+) : RateRepository {
     override suspend fun getExchangeRates() = exchangeRatesApi.getExchangeRates()
 }
